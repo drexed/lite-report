53bf6f7f7e20a856930470c40fe8e27f6ea3f1d8
Juan Gomez <j.gomez@drexed.com>
2019-07-07 11:46:03 -0400

    Rework encoders

--- spec/lite/report/helpers/encoders_spec.rb
+++ spec/lite/report/helpers/encoders_spec.rb
@@ -7,20 +7,9 @@ RSpec.describe Lite::Report::Helpers::Encoders do
   context 'when importing an array csv with encode data options' do
     it 'to be an array of arrays' do
       import!(
-        klass: array,
-        template: multi_array_2,
-        filename: :multi_headerless,
-        data_options: {
-          encode: ['UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '']
-        }
-      )
-    end
-
-    it 'to be an array' do
-      import!(
-        klass: array,
-        template: solo_array_2,
-        filename: :solo_headerless,
+        klass: Lite::Report::Array,
+        template: array,
+        filename: :headerless,
         data_options: {
           encode: ['UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '']
         }
